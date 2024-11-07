a10f0c00ba10b346db48c339fbe5d1aab045b2db
joschonarth <joschonarth@gmail.com>
2024-11-07 04:38:31 -0300

    chore: create project

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///daily_diet.db'
+
+db = SQLAlchemy(app)
+CORS(app)
+
