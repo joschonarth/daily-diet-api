b662d9c55f1100c5a8285ec68b1cc96fabe9ddad
joschonarth <joschonarth@gmail.com>
2024-11-07 05:03:45 -0300

    feat: add route to create meal

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///daily_diet.db'
@@ -12,5 +13,24 @@ class Meal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
-    date_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_time = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
     in_diet = db.Column(db.Boolean, nullable=False, default=True)
+
+
+@app.route('/api/meal/add', methods=['POST'])
+def add_meal():
+    data = request.json
+    if 'name' in data:
+        meal = Meal(
+            name=data["name"],
+            description=data.get("description", ""),
+            in_diet=data.get("in_diet", True)
+        )
+        db.session.add(meal)
+        db.session.commit()
+        return jsonify({"message": "Meal added successfully"}), 201
+    return jsonify({"message": "Invalid meal data"}), 400
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
