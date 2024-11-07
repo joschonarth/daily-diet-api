7dd91ab348ec5a444a74271a139366936a2dc4c1
joschonarth <joschonarth@gmail.com>
2024-11-07 04:39:55 -0300

    feat: create meal model

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///daily_diet.db'
 db = SQLAlchemy(app)
 CORS(app)
 
+class Meal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    date_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    in_diet = db.Column(db.Boolean, nullable=False, default=True)
