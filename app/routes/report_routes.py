b4a0b1087a3b669b47c3e5dae8f2632b8b9dc6d8
joschonarth <joschonarth@gmail.com>
2024-11-10 07:54:10 -0300

    fix: update week and month interval logic

--- app/routes/report_routes.py
+++ app/routes/report_routes.py
@@ -1,23 +1,28 @@
 from flask import Blueprint, jsonify, current_app, request
 from datetime import datetime, timedelta
 from app.models.models import Meal
+from calendar import monthrange
 
 report_bp = Blueprint('report', __name__)
 
 @report_bp.route('/api/report', methods=['GET'])
 def generate_report():      
-    end_date = datetime.now()
+    today  = datetime.now()
     
     period = request.args.get('period', 'week').lower()
 
     if period == 'day':
-        start_date = datetime.combine(end_date.date(), datetime.min.time())
+        start_date = datetime.combine(today.date(), datetime.min.time())
+        end_date = datetime.combine(today.date(), datetime.max.time())
     elif period == 'week':
-        start_date = (end_date - timedelta(weeks=1)).date()
-        start_date = datetime.combine(start_date, datetime.min.time())
+        start_of_week = today - timedelta(days=today.weekday() + 1)
+        start_date = datetime.combine(start_of_week.date(), datetime.min.time())
+        end_of_week = start_of_week + timedelta(days=6)
+        end_date = datetime.combine(end_of_week.date(), datetime.max.time())
     elif period == 'month':
-        start_date = (end_date - timedelta(days=30)).date()
-        start_date = datetime.combine(start_date, datetime.min.time())
+        start_date = datetime.combine(today.replace(day=1).date(), datetime.min.time())
+        last_day = monthrange(today.year, today.month)[1]
+        end_date = datetime.combine(today.replace(day=last_day).date(), datetime.max.time())
     else:
         return jsonify({"message": "Invalid range parameter"}), 400
     
@@ -41,7 +46,7 @@ def generate_report():
     if period == 'week':
         calorie_goal *= 7
     elif period == 'month':
-        calorie_goal *= 30
+        calorie_goal *= last_day
 
     progress = (total_calories / calorie_goal) * 100
     
