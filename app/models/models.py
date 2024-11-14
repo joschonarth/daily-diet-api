5a32827efeabeaab70848ea0b1fcf109582cab92
joschonarth <joschonarth@gmail.com>
2024-11-14 15:19:47 -0300

    feat: update create user route

--- app/models/models.py
+++ app/models/models.py
@@ -20,7 +20,8 @@ class Goals():
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
     meals = relationship('Meal', backref='user', cascade="all, delete")
     water_intakes = relationship('Water', backref='user', cascade="all, delete")
 
--- app/routes/users_routes.py
+++ app/routes/users_routes.py
@@ -7,6 +7,7 @@ users_bp = Blueprint('users_bp', __name__)
 @users_bp.route("/api/users", methods=['POST'])
 def create_user():
     data = request.json
+    username = data.get("username")
     email = data.get("email")
     password = data.get("password")
 
@@ -16,7 +17,7 @@ def create_user():
             return jsonify({"message": "Email already exists"}), 409
 
         hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
-        user = User(email=email, password=hashed_password)
+        user = User(username=username, email=email, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         return jsonify({"message": "User successfully registered"})
