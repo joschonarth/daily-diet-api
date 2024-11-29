a8a91be14ab0c238f00c2a0fa5995f5dc2d42bfa
joschonarth <joschonarth@gmail.com>
2024-11-29 18:12:03 -0300

    refactor: move user routes handlers to views

--- app/routes/users_routes.py
+++ app/routes/users_routes.py
@@ -1,48 +1,16 @@
 from flask import Blueprint, request, jsonify
-from app.models.models import db, User
-import bcrypt
-from flask_login import login_user, current_user, login_required, logout_user
+from flask_login import login_required, logout_user
+from app.views.users.users_views import create_user, login
 
 users_bp = Blueprint('users_bp', __name__)
 
 @users_bp.route("/api/users", methods=['POST'])
-def create_user():
-    data = request.json
-    username = data.get("username")
-    email = data.get("email")
-    password = data.get("password")
-
-    if email and password:
-        existing_user = User.query.filter_by(email=email).first()
-        if existing_user:
-            return jsonify({"message": "Email already exists"}), 409
-
-        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
-        user = User(username=username, email=email, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        return jsonify({"message": "User successfully registered"})
-    
-    return jsonify({"message": "Invalid credentials"}), 400
+def create_user_route():
+    return create_user(request.json)
 
 @users_bp.route("/api/users/login", methods=['POST'])
-def login():
-    data = request.json
-    email = data.get("email")
-    password = data.get("password")
-
-    if email and password:
-        user = User.query.filter_by(email=email).first()
-
-        if user and bcrypt.checkpw(str.encode(password), user.password):
-            login_user(user)
-
-            if current_user.is_authenticated:
-                return jsonify({"message": "Login successful", "user_id": current_user.id})
-            else:
-                return jsonify({"message": "Failed to authenticate user"}), 500
-    
-    return jsonify({"message": "Invalid credentials"}), 400
+def login_route():
+    return login(request.json)
 
 @users_bp.route("/api/users/logout", methods=['GET'])
 @login_required
---
+++ app/views/users/users_views.py
@@ -0,0 +1,41 @@
+from flask import request, jsonify
+from app.models.models import db, User
+import bcrypt
+from flask_login import login_user, current_user
+
+def create_user(data):
+    data = request.json
+    username = data.get("username")
+    email = data.get("email")
+    password = data.get("password")
+
+    if email and password:
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            return jsonify({"message": "Email already exists"}), 409
+
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, email=email, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "User successfully registered"})
+    
+    return jsonify({"message": "Invalid credentials"}), 400
+
+def login(data):
+    data = request.json
+    email = data.get("email")
+    password = data.get("password")
+
+    if email and password:
+        user = User.query.filter_by(email=email).first()
+
+        if user and bcrypt.checkpw(str.encode(password), user.password):
+            login_user(user)
+
+            if current_user.is_authenticated:
+                return jsonify({"message": "Login successful", "user_id": current_user.id})
+            else:
+                return jsonify({"message": "Failed to authenticate user"}), 500
+    
+    return jsonify({"message": "Invalid credentials"}), 400
